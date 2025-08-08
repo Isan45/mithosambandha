b6cce523e4de57a7cc2cd0a310445ae8248ed6ce
Digital Marketing Nepal <Stha.isan30@gmail.com>
2025-08-08 17:01:01 +0000

    One photo in 1 place please. we are using different photos.

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -127,7 +127,7 @@ export default function PricingPage() {
             </div>
             <div className="relative h-80 w-full rounded-lg shadow-xl overflow-hidden">
               <Image
-                src="https://firebasestorage.googleapis.com/v0/b/mitho-sambandha-c4959.firebasestorage.app/o/mitho-sambanda-3-.avif?alt=media&token=fff78dd1-f5e7-4db6-8c61-0d80a4ee5d76"
+                src="https://firebasestorage.googleapis.com/v0/b/mitho-sambandha-c4959.firebasestorage.app/o/mitho-sambandha-2.avif?alt=media&token=14b88d3a-ca2f-4078-b20d-e460edaaa7a4"
                 alt="Happy couple smiling"
                 fill
                 style={{ objectFit: 'fill' }}
