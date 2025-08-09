088817ee20b95d5f5ec91def53be920ac88d5792
Digital Marketing Nepal <Stha.isan30@gmail.com>
2025-08-09 11:34:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyBV_UssbqWWweViiyDR9Rp8M9mGKR5fHMc",
-  authDomain: "mitho-sambandha-c4959.firebaseapp.com",
-  projectId: "mitho-sambandha-c4959",
-  storageBucket: "mitho-sambandha-c4959.appspot.com",
-  messagingSenderId: "943158816698",
-  appId: "1:943158816698:web:565f4c1deb6161ee507783",
-  measurementId: "G-4VC7T24F14"
+  apiKey: 'AIzaSyChI2xnoM5a_Afg56r5Y5-n16TCEtFp86E',
+  authDomain: 'monospace-9.firebaseapp.com',
+  projectId: 'monospace-9',
+  storageBucket: 'monospace-9.appspot.com',
+  messagingSenderId: '943158816698',
+  appId: '1:943158816698:web:565f4c1deb6161ee507783',
+  measurementId: 'G-4VC7T24F14',
 };
 
 // Initialize Firebase
